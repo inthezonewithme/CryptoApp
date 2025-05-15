72dff837bcde86cc630fd14eccdf0219d400d514
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-05-15 18:29:22 +0300

    Initial commit to test and make sure everything works

---
+++ src/main/java/com/jesse/cryptoapp/CryptoappApplication.java
@@ -0,0 +1,13 @@
+package com.jesse.cryptoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoappApplication.class, args);
+	}
+
+}
